65326d31755f808580a38f318b4e6fc732239b4e
Andrew Chmutov <ulman.andre@gmail.com>
2023-08-21 20:31:27 +0200

    Credentials handling

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import selenium
+import json
 
 
 url = input("Enter the url of Pinterest board: ")
 
 # Firefox driver
 driver = './geckodriver'
+
+# Get email and password from json
+with open('info.json') as info_file:
+    info = json.load(info_file)
