de40b8e66be1f7479caaefc4105a18553aa9e351
Andrew Chmutov <ulman.andre@gmail.com>
2023-08-21 20:37:15 +0200

    Webdriver setup

--- script.py
+++ script.py
@@ -1,11 +1,21 @@
-import selenium
+# Default libraries
 import json
+
+# For browsing
+import selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 
 
 url = input("Enter the url of Pinterest board: ")
 
 # Firefox driver
-driver = './geckodriver'
+driver_path = './geckodriver'
+driver = webdriver.Firefox(
+    service=Service(driver_path),
+    options=Options()
+)
 
 # Get email and password from json
 with open('info.json') as info_file:
